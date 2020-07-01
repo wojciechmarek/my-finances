b6d4b6d2975d16e56f15a8fe12115857d3bfd966
Wojciech Marek <marek.wojciech@outlook.com>
2020-07-01 21:00:26 +0200

    Fake stock prices generator

--- MyFinances.Stock/Service/StockApi.cs
+++ MyFinances.Stock/Service/StockApi.cs
@@ -1,27 +1,100 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
+using MyFinances.Common.Exceptions;
+using MyFinances.Common.Models.Stock;
 
 namespace MyFinances.Stock.Service
 {
     public class StockApi
     {
-        private HttpClient httpClient;
-
-        public StockApi()
-        {
-            httpClient = new HttpClient();
-        }
+        private IList<StockModel> _prices;
 
         public object ReturnListOfStocks()
         {
-            return new object();
+            _prices = new List<StockModel>()
+            {
+                new StockModel("Alior", "ALR", 16),
+                new StockModel("CCC", "CCC", 59),
+                new StockModel("CD Projekt", "CDR", 403),
+                new StockModel("Cyfrowy Polsat", "CPS", 25),
+                new StockModel("Dino", "DNP", 199),
+                new StockModel("Jastrzębska Spółka Węglowa", "JSW", 18),
+                new StockModel("HGHM", "KGH", 92),
+                new StockModel("Lotos", "LTS", 60),
+                new StockModel("LPP", "LPP", 6119),
+                new StockModel("mBank", "MBK", 234),
+                new StockModel("Orange Polska", "OPL", 6),
+                new StockModel("Pekao", "PEO", 53),
+                new StockModel("PGE", "PGE", 6),
+                new StockModel("PGNiG", "PGN", 4),
+                new StockModel("Orlen", "PKN", 63),
+                new StockModel("PKO BP", "PKO", 23),
+                new StockModel("Play", "PLY", 31),
+                new StockModel("PZU", "PZU", 28),
+                new StockModel("Santander", "SPL", 175),
+                new StockModel("Tauron", "TPE", 2),
+            };
         }
 
-        public object GetInfoAboutStock(string symbol)
+        public IList<Common.Models.Stock.Stock> SearchStock(string symbolOrName)
         {
-            return new object();
+            IList<Common.Models.Stock.Stock> resultStocks = new List<Common.Models.Stock.Stock>();
+           
+
+            var resultQuery = _prices
+                .Where(x => x.Name.Contains(symbolOrName) || x.Symbol.Contains(symbolOrName)).ToList();
+
+            if (resultQuery == null) throw new StockNotFoundException();
+
+            foreach (var stock in resultQuery)
+            {
+                var minPrice = stock.Price - 1;
+                var maxPrice = stock.Price + 1;
+
+                resultStocks.Add(new Common.Models.Stock.Stock()
+                {
+                    Name = stock.Name,
+                    Symbol = stock.Symbol,
+                    LastPrice = (decimal)GenerateFakePrice(stock.Price)
+                });
+            }
+
+            return resultStocks;
+        }
+
+        public Common.Models.Stock.StockHistory StockInfo(string symbol)
+        {
+            var resultQuery = _prices.FirstOrDefault(x => x.Symbol.Contains(symbol));
+
+            if (resultQuery == null) throw new StockNotFoundException();
+
+            return new StockHistory()
+            {
+                Name = resultQuery.Name,
+                Symbol = resultQuery.Symbol,
+                Price = new List<decimal>()
+                {
+                    (decimal) GenerateFakePrice(resultQuery.Price),
+                    (decimal) GenerateFakePrice(resultQuery.Price),
+                    (decimal) GenerateFakePrice(resultQuery.Price),
+                }
+            };
+
+
+        }
+
+        private double GenerateFakePrice(double price)
+        {
+            Random random = new Random();
+
+            var minPrice = price - 1;
+            var maxPrice = price + 1;
+
+            return random.NextDouble() * (maxPrice - minPrice) + minPrice);
         }
     }
 }
---
+++ MyFinances.Stock/Service/StockModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFinances.Stock.Service
+{
+    public class StockModel
+    {
+        public StockModel(string name, string symbol, double price)
+        {
+            Name = name;
+            Symbol = symbol;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public double Price { get; set; }
+    }
+}
