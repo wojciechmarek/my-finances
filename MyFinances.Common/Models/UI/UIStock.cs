9df93b14129682682200027c522cffd66aed2f24
Wojciech Marek <marek.wojciech@outlook.com>
2020-07-01 19:45:23 +0200

    UI done

---
+++ MyFinances.Common/Models/UI/UIStock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFinances.Common.Models.UI
+{
+    public class UIStock
+    {
+        public UIStock(DateTime time, decimal price)
+        {
+            Time = time;
+            Price = price;
+        }
+
+        public DateTime Time { get; set; }
+        public decimal Price { get; set; }
+    }
+}
